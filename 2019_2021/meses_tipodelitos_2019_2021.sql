5f11e4a6c67f2371c943f76722dd56534fd0f35b
SergioEscobedo <137465266+SergioEscobedo@users.noreply.github.com>
2023-09-25 20:29:15 -0600

    Add files via upload

---
+++ 2019_2021/meses_tipodelitos_2019_2021.sql
@@ -0,0 +1,37 @@
+WITH DelitosPorMes AS (
+    SELECT
+        anio_hecho,
+        mes_hecho,
+        delito, 
+        COUNT(*) AS cantidad_delito
+    FROM
+        datos_delitos_2019_2021
+    WHERE
+        anio_hecho >= '2019'
+    GROUP BY
+        anio_hecho,
+        mes_hecho,
+        delito
+),
+DelitosRanking AS (
+    SELECT
+        anio_hecho,
+        mes_hecho,
+        delito,
+        cantidad_delito,
+        ROW_NUMBER() OVER (PARTITION BY anio_hecho, mes_hecho ORDER BY cantidad_delito DESC) AS ranking
+    FROM
+        DelitosPorMes
+)
+SELECT
+    anio_hecho,
+    mes_hecho,
+    delito AS tipo_delito_mas_comun,
+    cantidad_delito AS cantidad_delito_mas_comun
+FROM
+    DelitosRanking
+WHERE
+    ranking = 1
+ORDER BY
+    anio_hecho,
+    mes_hecho DESC;
